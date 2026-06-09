2c00fa5d81af6bcad8640640d372a5a5054fd12e
Tugtech <gowhorodu@palate.ng>
2026-06-09 15:08:55 -0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,10 +34,10 @@
             Strategic Engagement &nbsp; <span class="font-sans-serif">&rarr;</span>
           </button>
           
-          <a href="#" class="media-overview-trigger">
+          {{-- <a href="#" class="media-overview-trigger">
             <svg fill="currentColor" class="gold-brand-text" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/></svg>
             <span>ATICC INSTITUTIONAL<br>OVERVIEW</span>
-          </a>
+          </a> --}}
         </div>
 
       </div>
